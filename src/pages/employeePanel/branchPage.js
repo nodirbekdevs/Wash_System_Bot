99fdf6c798dd049dd37fc02e546579d33d06dac8
nodirdevs <nodirbekdevs@mail.ru>
2023-02-08 03:21:42 +0500

    employees cars page ended

--- src/pages/employeePanel/branchPage.js
+++ src/pages/employeePanel/branchPage.js
@@ -8,7 +8,7 @@ const {date, bio} = require('./../../helpers/utils')
 const ebs0 = async (bot, chat_id, lang) => {
   const employee = await getEmployee({telegram_id: chat_id}), manager = await getManager({telegram_id: employee.manager}),
     branch = await getBranch({owner: manager.owner, manager: manager.telegram_id, name: manager.branch}),
-    kbb = (lang === kb.language.uz) ? keyboard.manager.pages.uz : keyboard.manager.pages.ru,
+    kbb = (lang === kb.language.uz) ? keyboard.employee.pages.uz : keyboard.employee.pages.ru,
     started_at = date(branch.created_at)
 
   const data = {
